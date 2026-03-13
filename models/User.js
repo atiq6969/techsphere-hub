3c9d48a4221ece70c60769630e96955cb30906b4
Mohammad Atiq <108713519+atiq6969@users.noreply.github.com>
2026-03-14 00:17:07 +0530

    Create User.js model for user authentication

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    interests: {
+        type: [String],
+        default: [],
+    },
+}, { timestamps: true });
+
+// Hash password before saving user model
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
