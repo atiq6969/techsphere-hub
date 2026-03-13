a1832a08b377f7d10975e6347a4c03c078181487
Mohammad Atiq <108713519+atiq6969@users.noreply.github.com>
2026-03-14 00:16:14 +0530

    Create News.js model file for the news schema

---
+++ models/News.js
@@ -0,0 +1,26 @@
+// News.js
+
+const mongoose = require('mongoose');
+
+const newsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    author: {
+        type: String,
+        required: true
+    }
+});
+
+const News = mongoose.model('News', newsSchema);
+
+module.exports = News;
